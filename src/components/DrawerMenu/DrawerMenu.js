fb9027cce6a18462157ef00e65d2f348585a7b28
estherjj <wjswls109@naver.com>
2021-09-15 22:57:50 +0900

    DrawerMenu 컴포넌트 수정

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -10,62 +10,55 @@ function DrawerMenu(props){
 
   return(
     <>
-    <section className={styles.section} ref={drawer_section} onClick={(e)=>{
+    <aside className={styles.sm_only} ref={drawer_section} onClick={(e)=>{
       if(e.target === drawer_section.current){
         props.setDrawerShow(false)
       }
     }}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={styles.col_sm_3}>
-            {/* 여기 */}
-            <div className={`${styles.sm_only} ${styles.drawer_menu}`}>
-              <div className={styles.drawer_menu_header}>
-                <div className={styles.logo}>
-                  <img src="./images/logo.svg" alt="내일의 집"/>
-                </div>
-                {/* 로그인 전 */}
-                <div className={styles.user_link}>
-                  <Link to="/login" className={`${styles.btn_login} ${styles.btn_primary} ${styles.btn_40}`} onClick={()=>{
-                    props.setDrawerShow(false)
-                  }}>
-                    로그인
-                  </Link>
-                  <Link to="/join" className={`${styles.btn_join} ${styles.btn_outlined} ${styles.btn_40}`}  onClick={()=>{
-                    props.setDrawerShow(false)
-                  }}>
-                    회원가입
-                  </Link>
-                </div>
-
-                {/* 로그인 후 */}
-                {/* <div className={styles.user}>
-                  <div className={styles.user_profile}>
-                    <img src="./images/img-user-default.png" alt="유저 프로필 사진"/>
-                  </div>
-                  <span className={styles.user_name}>김내일</span>
-                </div> */}
-              </div>
-              <nav className={styles.drawer_menu_category}>
-                <h2>카테고리</h2>
-                <Store fill="#3f4150" className={`${styles.icon} ${styles.storeIcon}`}/>
-                <ul className={styles.drawer_menu_category_list}>
-                  {
-                    category.map(item=> {
-                      return (<li className={styles.drawer_menu_category_item} onClick={()=>{
-                        props.setDrawerShow(false)
-                      }}>
-                        <Link to="/category">{item}</Link>
-                      </li>)
-                    })
-                  }
-                </ul>
-              </nav>
-            </div>
+      <div className={styles.drawer_menu}>
+        <div className={styles.drawer_menu_header}>
+          <div className={styles.logo}>
+            <img src="./images/logo.svg" alt="내일의 집"/>
           </div>
+          {/* 로그인 전 */}
+          <div className={styles.user_link}>
+            <Link to="/login" className={`${styles.btn_login} ${styles.btn_primary} ${styles.btn_40}`} onClick={()=>{
+              props.setDrawerShow(false)
+            }}>
+              로그인
+            </Link>
+            <Link to="/join" className={`${styles.btn_join} ${styles.btn_outlined} ${styles.btn_40}`}  onClick={()=>{
+              props.setDrawerShow(false)
+            }}>
+              회원가입
+            </Link>
+          </div>
+
+          {/* 로그인 후 */}
+          {/* <div className={styles.user}>
+            <div className={styles.user_profile}>
+              <img src="./images/img-user-default.png" alt="유저 프로필 사진"/>
+            </div>
+            <span className={styles.user_name}>김내일</span>
+          </div> */}
         </div>
+        <nav className={styles.drawer_menu_category}>
+          <h2>카테고리</h2>
+          <Store fill="#3f4150" className={`${styles.icon} ${styles.storeIcon}`}/>
+          <ul className={styles.drawer_menu_category_list}>
+            {
+              category.map(item=> {
+                return (<li className={styles.drawer_menu_category_item} onClick={()=>{
+                  props.setDrawerShow(false)
+                }}>
+                  <Link to="/category">{item}</Link>
+                </li>)
+              })
+            }
+          </ul>
+        </nav>
       </div>
-    </section>
+    </aside>
     </>
   )
 }
