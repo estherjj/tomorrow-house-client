9606fcdf507bc7fe4e5b27c0a68b824b8ef87d8b
estherjj <wjswls109@naver.com>
2021-09-23 22:21:46 +0900

    carousel 링크태그 추가

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
+
 
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import styles from './Carousel.module.scss';
+
 
 
 function Carousel(){
@@ -22,20 +25,28 @@ function Carousel(){
     <>
     <Slider {...settings} dotsClass={styles.slick_dots}>
       <div className={styles.carousel}>
-        <img src="images/img-carousel01.jpg" alt="캐로샐 1" className={styles.sm_only}/>
-        <img src="images/img-carousel01-wide.jpg" alt="캐로샐 1" className={styles.sm_hidden}/>
+        <Link to="/">
+          <img src="images/img-carousel01.jpg" alt="캐로샐 1" className={styles.sm_only}/>
+          <img src="images/img-carousel01-wide.jpg" alt="캐로샐 1" className={styles.sm_hidden}/>
+        </Link>
       </div>
       <div className={styles.carousel}>
-        <img src="images/img-carousel02.jpg" alt="캐로샐 2" className={styles.sm_only}/>
-        <img src="images/img-carousel02-wide.jpg" alt="캐로샐 2" className={styles.sm_hidden}/>
+        <Link to="/">
+          <img src="images/img-carousel02.jpg" alt="캐로샐 2" className={styles.sm_only}/>
+          <img src="images/img-carousel02-wide.jpg" alt="캐로샐 2" className={styles.sm_hidden}/>
+        </Link>
       </div>
       <div className={styles.carousel}>
-        <img src="images/img-carousel03.jpg" alt="캐로샐 3" className={styles.sm_only}/>
-        <img src="images/img-carousel03-wide.jpg" alt="캐로샐 3" className={styles.sm_hidden}/>
+        <Link to="/">
+          <img src="images/img-carousel03.jpg" alt="캐로샐 3" className={styles.sm_only}/>
+          <img src="images/img-carousel03-wide.jpg" alt="캐로샐 3" className={styles.sm_hidden}/>
+        </Link>
       </div>
       <div className={styles.carousel}>
-        <img src="images/img-carousel04.jpg" alt="캐로샐 4" className={styles.sm_only}/>
-        <img src="images/img-carousel04-wide.jpg" alt="캐로샐 4" className={styles.sm_hidden}/>
+        <Link to="/">
+          <img src="images/img-carousel04.jpg" alt="캐로샐 4" className={styles.sm_only}/>
+          <img src="images/img-carousel04-wide.jpg" alt="캐로샐 4" className={styles.sm_hidden}/>
+        </Link>
       </div>
     </Slider>
     </>
