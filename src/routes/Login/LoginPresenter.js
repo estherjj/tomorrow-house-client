d620a3052fce5278e17ba3e6a1f78ff8f3d50d34
estherjj <wjswls109@naver.com>
2021-09-29 23:07:52 +0900

    login 페이지 기본 ui 완성

--- src/routes/Login/LoginPresenter.js
+++ src/routes/Login/LoginPresenter.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styles from './Login.module.scss';
 
 function LoginPresenter(){
   return (
-    <div>
-      Login !!
-    </div>
+    <section>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.col_sm_4}>
+            <div className={styles.login}>
+              <h2>로그인</h2>
+              <form method="get" action="/" className={styles.form}>
+                <input type="email" placeholder="아이디(이메일)" name="id" required />
+                <input type="password" placeholder="비밀번호" name="password" required />
+                <button type="button" className={`${styles.btn_primary} ${styles.btn_55} ${styles.btn_submit}`} >로그인</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
