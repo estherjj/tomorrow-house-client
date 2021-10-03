9fbbaa7298f90ffbba2116541bd36bdaeb3fd178
estherjj <wjswls109@naver.com>
2021-10-03 23:56:02 +0900

    수정: onSilentRefresh 함수 삭제됨에 따른 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Router from "./Router";
-import {onSilentRefresh} from '../auth';
 
 function App() {
-  useEffect(()=>{
-    onSilentRefresh();
-  }, [])
 
   return (
     <div className="App">
